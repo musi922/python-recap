c6c2729034badbcfa7937fe7ad22e55313ec43d1
Richard Musime <richardmusime6@gmail.com>
2026-06-13 15:00:53 +0200

    Feat: added the plot

--- matplotlib.py
+++ matplotlib.py
@@ -6,3 +6,12 @@ plt.scatter(df.index,df["Gender"])
 plt.ylabel("GENDER")
 plt.xlabel("INDEX")
 plt.show()
+
+
+
+
+#General Formula
+#1. Library           -> import pandas as pd
+#2. Path/File         -> variablefile = r"C:\users"
+#3. Read              -> variable2 = pd.read_csv(variablefile)
+#4. Display           -> print(variable2)
