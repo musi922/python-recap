3c7c7d33f840b431280316a74cd843b42c0dfda6
Richard Musime <richardmusime6@gmail.com>
2026-06-20 18:21:32 +0200

    added changes:

---
+++ revision.py
@@ -0,0 +1,54 @@
+import math
+import time
+st=1
+total=0
+for st in range(2)
+    marks=float(input("Enter the marks"))
+    total+=marks
+    average=total/2
+    squareroot=math.sqrt(total)
+print("Total",total)
+
+print(time.ctime())
+
+# loops
+i=1
+while i<=10
+print(i)
+i+=1
+
+#detector
+nationid=input("Enter your age")
+detectnu = nationid[5]
+checkdatof birth = int(nationid[1:4])
+
+#even and odd
+num = int(input("Enter Number"))
+if num>0
+   for i in range(0,10)
+   if i%2 ===0
+   print(i)
+else
+ for i in range(0,10)
+  if i%2 !=0
+  print(i)
+
+#factorial
+num = int(input("Enter Number"))
+factoria = 1
+if num>0
+  for i in range(1,num+1)
+    factorial = factorial * i
+    print(Factorial)
+
+
+# calcualtor
+def calc(num1,num2,oprator):
+    if operator === '+'
+    print(num1+num2)
+    elif operator === '-'
+    print(num1-num2)
+    elif operator === '+'
+    print(num1+num2)
+    elif operator === '+'
+    print(num1+num2)
